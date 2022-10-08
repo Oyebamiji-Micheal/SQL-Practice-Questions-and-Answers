3f2b02552e7a73206ef2795cec2cae250e839950
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2022-10-08 21:35:29 +0100

    Add solution 'Type of Triangle'

---
+++ queries/Type_of_Triangle.sql
@@ -0,0 +1,11 @@
+SELECT 
+    CASE
+    WHEN 
+        (A + B <= C) OR 
+        (A + C <= B) OR 
+        (B + C <= A) THEN 'Not A Triangle'
+    WHEN A = B AND B = C THEN 'Equilateral'
+    WHEN A = B OR A = C OR B = C THEN 'Isosceles'
+    WHEN A != B AND B != C THEN 'Scalene'
+    END AS 'Triangle'
+FROM TRIANGLES;
