3409cd398ca3f6b24493d71ce4eb65d5727c6f43
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2022-10-11 12:03:15 +0100

    Add new solution 'Customer Who Visited but Did Not Make Any Transactions'

---
+++ queries/Customer_Who_Visited_but_Did_Not_Make_Any_Transactions.sql
@@ -0,0 +1,8 @@
+SELECT 
+    customer_id, 
+    COUNT(*) AS count_no_trans 
+FROM Visits
+LEFT JOIN Transactions 
+    ON Visits.visit_id = Transactions.visit_id
+WHERE transaction_id IS NULL
+GROUP BY customer_id;
